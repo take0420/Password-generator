a9537dfae718a802e53d78e49cde9b71348623a5
take0420 <zakisen81@gmail.com>
2025-01-04 10:30:57 +0900

    update: ランダムな8文字を表示する処理

--- main.js
+++ main.js
@@ -2,10 +2,23 @@
 
 {
   const slider = document.getElementById('slider');
+  const btn = document.getElementById('btn');
 
   slider.addEventListener('input', () => {
     const passwordLength = document.getElementById('password-length');
 
     passwordLength.textContent = slider.value;
+  });
+
+  btn.addEventListener('click', () => {
+    const result = document.getElementById('result');
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    let password = '';
+
+    for (let i = 0; i < 8; i++) {
+      password += letters[Math.floor(Math.random() * 26)];
+    }
+
+    result.textContent = password;
   });
 }
