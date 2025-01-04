b7091cdb22d8fe0e42523bf41305c35a067dbe6f
take0420 <zakisen81@gmail.com>
2025-01-04 10:24:16 +0900

    feat: パスワードの長さの表示を動的に変化できる機能

---
+++ main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+{
+  const slider = document.getElementById('slider');
+
+  slider.addEventListener('input', () => {
+    const passwordLength = document.getElementById('password-length');
+
+    passwordLength.textContent = slider.value;
+  });
+}
