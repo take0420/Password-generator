14af6120b9143a34f47540988657ad0851d28dab
take0420 <zakisen81@gmail.com>
2025-01-04 10:42:50 +0900

    feat: ランダム文字列に記号を追加

--- main.js
+++ main.js
@@ -13,13 +13,19 @@
   btn.addEventListener('click', () => {
     const result = document.getElementById('result');
     const numbersCheckbox = document.getElementById('numbers-checkbox');
+    const symbolsCheckbox = document.getElementById('symbols-checkbox');
     const letters = 'abcdefghijklmnopqrstuvwxyz';
     const numbers = '0123456789';
+    const symbols = '!#$%&()';
     let password = '';
     let seed = letters + letters.toUpperCase();
 
     if (numbersCheckbox.checked) {
       seed += numbers;
+    }
+
+    if (symbolsCheckbox.checked) {
+      seed += symbols;
     }
 
     for (let i = 0; i < slider.value; i++) {
